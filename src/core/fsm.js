7c93081694fbc1a2d2f4d4d4e770963bf3a6c8a9
manyacap <comercial.allpower@gmail.com>
2025-04-23 19:38:23 -0300

    Create fsm.js

---
+++ src/core/fsm.js
@@ -0,0 +1,18 @@
+export class AliciaFSM {
+  // ... Constructor y estados previos
+  
+  setupEventListeners() {
+    window.addEventListener('voice-toggle', () => {
+      if (this.currentState === this.states.IDLE) {
+        this.transitionTo(this.states.LISTENING);
+      }
+    });
+
+    window.addEventListener('alicia-statechange', (e) => {
+      const uiUpdateEvent = new CustomEvent('update-ui', {
+        detail: { state: e.detail.state }
+      });
+      window.dispatchEvent(uiUpdateEvent);
+    });
+  }
+}
