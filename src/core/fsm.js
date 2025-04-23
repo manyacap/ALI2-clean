7fbc2bfdb0b7e0fc98f7ff453d7a7333c0898dab
manyacap <comercial.allpower@gmail.com>
2025-04-23 12:45:38 -0300

    Update fsm.js

--- src/core/fsm.js
+++ src/core/fsm.js
@@ -1,1 +1,33 @@
 
+export const STATES = {
+  IDLE: 'idle',
+  LISTENING: 'listening',
+  PROCESSING: 'processing',
+  SPEAKING: 'speaking'
+};
+
+const VALID = {
+  idle: ['listening'],
+  listening: ['processing', 'idle'],
+  processing: ['speaking', 'idle'],
+  speaking: ['idle']
+};
+
+class FSM {
+  constructor() {
+    this.state = STATES.IDLE;
+  }
+  init() {
+    console.debug('[FSM] init:', this.state);
+  }
+  async transitionTo(next) {
+    if (VALID[this.state].includes(next)) {
+      this.state = next;
+      console.debug('[FSM] to:', next);
+      return true;
+    }
+    return false;
+  }
+}
+
+export default new FSM();
