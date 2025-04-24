a2ba31c894292242f37a6ea75af8a212971eb671
manyacap <comercial.allpower@gmail.com>
2025-04-24 15:46:56 -0300

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,5 @@
-/** @type {import("prettier").Config} */
 module.exports = {
-  printWidth: 80,        // ancho máximo de línea
-  singleQuote: true,     // comillas simples
-  trailingComma: 'es5'   // comas finales donde lo permite ES5
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
 };
